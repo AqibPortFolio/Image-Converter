a6e19ca78cddef78cc51bb671fbf8ff88dba231f
AqibPortFolio <aqibbuttportfolio@gmail.com>
2024-06-28 10:42:16 +0500

    Nav Footer And Convert Update

--- src/components/converter/Converter.js
+++ src/components/converter/Converter.js
@@ -79,32 +79,31 @@ const Converter = () => {
     <div className="bg-slate-50 border-b-2 flex items-center justify-center p-4">
       <div className="w-full max-w-md p-4 bg-white rounded-lg shadow-md">
         <h2 className="text-lg font-semibold mb-4">Upload Image</h2>
-        <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
-          <div className="flex flex-col items-center justify-center pt-5 pb-6">
-            <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
-              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
+        <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50  hover:bg-gray-100  relative">
+          {file ? (
+            <img src={URL.createObjectURL(file)} alt="Uploaded" className="absolute inset-0 w-full h-full object-contain  rounded-lg" />
+          ) : (
+            <div className="flex flex-col items-center justify-center pt-5 pb-6">
+              <svg class="w-8 h-8 mb-4 text-gray-500 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
             </svg>
-            <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
-            <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
-          </div>
+              <p className="mb-2 text-sm text-gray-500 "><span className="font-semibold">Click to upload</span> or drag and drop</p>
+              <p className="text-xs text-gray-500 ">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+            </div>
+          )}
           <input id="dropzone-file" type="file" className="hidden" onChange={handleFileChange} />
         </label>
 
         <p className="text-sm text-gray-600 mt-2">{status}</p>
 
         {file && (
-          <>
-            <div className="mt-4">
-              <img src={URL.createObjectURL(file)} alt="Uploaded" className="w-full h-auto rounded-md" />
-            </div>
-            <button
-              onClick={handleConvertToMp3}
-              className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
-              disabled={isLoading} // Disable button when loading
-            >
-              Convert to MP3
-            </button>
-          </>
+          <button
+            onClick={handleConvertToMp3}
+            className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+            disabled={isLoading} 
+          >
+            Convert to MP3
+          </button>
         )}
 
         {isLoading && (
@@ -137,8 +136,6 @@ const Converter = () => {
           </div>
         )}
       </div>
-
-      
     </div>
   );
 };
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,76 +1,132 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import metaviz_logo from "../assets/Logo-Metaviz-Pro.png"; // Replace this with the correct path to your image
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-<header className=" w-full border ">
-  <nav className="bg-white border-gray-200 py-2.5 dark:bg-gray-900">
-    <div className="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto">
-      <Link to="/" className="flex items-center">
-        <img src={metaviz_logo} className=" h-16 mr-3 sm:h-16" alt="Landwind Logo" />
-      </Link>
-      <div
-        className="items-center justify-between hidden w-full lg:flex lg:w-auto lg:order-1"
-        id="mobile-menu-2"
-      >
-        <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-          <li>
-            <Link
-              to="#"
-              className="block py-2 pl-3 pr-4 text-white bg-purple-700 rounded lg:bg-transparent lg:text-purple-700 lg:p-0 dark:text-white"
-              aria-current="page"
+    <header className="w-full border bg white">
+      <nav className="bg-white border-gray-200 py-2.5">
+        <div className="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto">
+          <Link to="/" className="flex items-center">
+            <img
+              src={metaviz_logo}
+              className="h-16 mr-3 sm:h-16"
+              alt="Landwind Logo"
+            />
+          </Link>
+          <div className="lg:hidden">
+            <button
+              onClick={toggleMenu}
+              className="block lg:hidden focus:outline-none"
             >
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="#"
-              className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-            >
-              Company
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="#"
-              className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-            >
-              Marketplace
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="#"
-              className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-            >
-              Features
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="#"
-              className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-            >
-              Team
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="#"
-              className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-            >
-              Contact
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-</header>
-
-
+              <svg
+                className="h-6 w-6"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                {isOpen ? (
+                  <path
+                    d="M6 18L18 6M6 6l12 12"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                ) : (
+                  <path
+                    d="M4 6H20M4 12H20M4 18H20"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                )}
+              </svg>
+            </button>
+          </div>
+          <div
+            className={`${
+              isOpen ? "block" : "hidden"
+            } items-center justify-between w-full lg:flex lg:w-auto lg:order-1`}
+            id="mobile-menu-2"
+          >
+            <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+              <li>
+                <Link
+                  to="/"
+                  className="block py-2 pl-3 pr-4   rounded lg:bg-transparent text-purple-700 lg:p-0 "
+                  aria-current="page"
+                >
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="https://metaviz.pro/about-us/"
+                  className="block py-2 pl-3 pr-4   rounded lg:bg-transparent hover:text-purple-700 lg:p-0  " >
+                  About Us
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="https://metaviz.pro/our-services/"
+                  className="block py-2 pl-3 pr-4   rounded lg:bg-transparent hover:text-purple-700 lg:p-0 "
+                >
+                  Our Services
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="https://bd.metaviz.pro/"
+                  className="block py-2 pl-3 pr-4   rounded lg:bg-transparent hover:text-purple-700 lg:p-0  "
+                >
+                  Brilliant Directories
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="https://metaviz.pro/portfolio/"
+                  className="block py-2 pl-3 pr-4   rounded lg:bg-transparent hover:text-purple-700 lg:p-0  "
+                >
+                  Portfolio
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="https://metaviz.pro/pricing/"
+                  className="block py-2 pl-3 pr-4   rounded lg:bg-transparent hover:text-purple-700 lg:p-0  "
+                >
+                  Pricing
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="https://metaviz.pro/contact-us/"
+                  className="block py-2 pl-3 pr-4   rounded lg:bg-transparent hover:text-purple-700 lg:p-0  "
+                >
+                  Contact Us
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="https://metaviz.pro/contact-us/"
+                  className="block py-2 pl-3 pr-4   rounded lg:bg-transparent hover:text-purple-700 lg:p-0  "
+                >
+                  Get Quote
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
   );
 };
 
